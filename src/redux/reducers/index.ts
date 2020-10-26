f9022b7d50f5adb8da365945096c07faf934ac28
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-10-26 12:45:52 +0100

    [Antact] combinedReducers implementation

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import {
+    appComponent
+} from './app';
+
+const combinedReducers = combineReducers({
+    // App reducers...
+    appComponent
+});
+
+export default combinedReducers;
